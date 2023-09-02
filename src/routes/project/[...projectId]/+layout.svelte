3af59c814df8d81ab80a8a5bd10076a503e67047
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-02 12:22:51 +0530

    feat: projects page, bg image
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

---
+++ src/routes/project/[...projectId]/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { Home, Send, History } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+
+	let paneItems = [
+		{
+			itemId: 0,
+			icon: Home,
+			hoverText: 'Project Home',
+			action: null
+		},
+		{
+			itemId: 1,
+			icon: Send,
+			hoverText: 'Mint',
+			action: null
+		},
+		{
+			itemId: 2,
+			icon: History,
+			hoverText: 'Mint History',
+			action: null
+		}
+	];
+</script>
+
+<div class="flex h-screen shadow-lg">
+	<div
+		class="w-16 bg-white p-4 shadow-md border-r-2 border-green-400 flex flex-col justify-start items-center"
+	>
+		{#each paneItems as item (item.itemId)}
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<Button variant="ghost">
+						<svelte:component this={item.icon} />
+					</Button>
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>{item.hoverText}</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		{/each}
+	</div>
+</div>
+
+<slot />
---
+++ src/routes/project/[...projectId]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+    import {page} from "$app/stores";
+
+    const {projectId} = $page.params;
+
+    console.log(projectId);
+    
+
+</script>
+
+<h2>For ${projectId}</h2>
