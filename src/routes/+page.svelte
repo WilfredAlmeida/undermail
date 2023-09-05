1b33b878c07f99fcb0f41dd51c0edb356d072480
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-05 11:50:49 +0530

    feat: landing page skeleton
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,45 @@
-<div>Hello</div>
+<div class="bg-blue-500 text-white py-10">
+    <div class="container mx-auto text-center">
+      <h1 class="text-4xl font-bold">Welcome to UnderMail</h1>
+      <p class="mt-4 text-lg">The Future of Email Marketing on Solana Blockchain</p>
+    </div>
+  </div>
+  
+  <section class="py-16">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <!-- Feature 1 -->
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <h2 class="text-xl font-semibold">Secure & Decentralized</h2>
+          <p class="mt-4">With Solana's blockchain technology, your email campaigns are secure and decentralized.</p>
+        </div>
+  
+        <!-- Feature 2 -->
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <h2 class="text-xl font-semibold">Low Transaction Fees</h2>
+          <p class="mt-4">Enjoy cost-effective email marketing with minimal transaction fees.</p>
+        </div>
+  
+        <!-- Feature 3 -->
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <h2 class="text-xl font-semibold">User-Friendly Interface</h2>
+          <p class="mt-4">Our intuitive interface makes managing your email campaigns a breeze.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="bg-blue-500 py-16 text-white">
+    <div class="container mx-auto text-center">
+      <h2 class="text-2xl font-semibold">Ready to get started with UnderMail?</h2>
+      <p class="mt-4 text-lg">Join us today and revolutionize your email marketing!</p>
+      <button class="mt-8 bg-white text-blue-500 hover:bg-blue-700 py-2 px-6 rounded-full font-semibold">Sign Up</button>
+    </div>
+  </section>
+  
+  <footer class="bg-gray-200 py-6">
+    <div class="container mx-auto text-center">
+      <p>&copy; 2023 UnderMail. All rights reserved.</p>
+    </div>
+  </footer>
+  
