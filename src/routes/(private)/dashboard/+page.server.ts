5f96c3ac8959276c456aba3a8c30c4af6b4a7754
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-06 14:45:57 +0530

    feat: user creation after login
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/(private)/dashboard/+page.server.ts
+++ src/routes/(private)/dashboard/+page.server.ts
@@ -4,8 +4,8 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async({locals})=>{
     /* @ts-ignore */
     const {data} = await locals.supabase.auth.getSession();
-    console.log("SESSION");
-    console.log(data);
+    // console.log("SESSION");
+    // console.log(data);
     
     if(!data.session){
         throw redirect(303, "/login")
--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,8 +1,9 @@
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ locals }) => {
+    /* @ts-ignore */
     await locals.supabase.auth.signOut()
 
-    throw redirect(303, "/")
+    throw redirect(303, "/login")
 
 };
