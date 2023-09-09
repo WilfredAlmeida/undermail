3fef41d0cb4f3bd6411ae95362b55c59dd058fa2
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-10 01:01:39 +0530

    feat: mint history display
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

---
+++ src/routes/(private)/project/[...projectId]/history/+page.server.ts
@@ -0,0 +1,22 @@
+export const load = async({url, locals})=>{
+
+    const {supabase} = locals;
+
+    const projectId = url.pathname.split("/")[2]
+
+    const dbRes = await supabase.from("mints").select("*").eq("project_id", projectId)
+
+    console.log(dbRes.data);
+    
+    if(dbRes.data.length ===0){
+        return {
+            error: "No mints found or project doesn't exist"
+        }
+    }
+
+    return {
+        mints: dbRes.data.reverse()
+    }
+
+
+}
--- src/routes/(private)/project/[...projectId]/history/+page.svelte
+++ src/routes/(private)/project/[...projectId]/history/+page.svelte
@@ -1,62 +1,73 @@
 <script>
 	import { onMount } from 'svelte';
 	import * as Card from '$lib/components/ui/card';
+	export let data;
 
-	let items = Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`); // Initial list of items
-	let isLoading = false; // Indicates if more items are being loaded
+	// let items = Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`); // Initial list of items
+	// let isLoading = false; // Indicates if more items are being loaded
 
-	// Function to load more items
-	async function loadMoreItems() {
-		isLoading = true;
-		// Simulate loading more items (you can replace this with an actual API call)
-		await new Promise((resolve) => setTimeout(resolve, 1000));
-		const newItems = Array.from({ length: 5 }, (_, i) => `Item ${items.length + i + 1}`);
-		items = [...items, ...newItems];
-		isLoading = false;
-	}
+	// // Function to load more items
+	// async function loadMoreItems() {
+	// 	isLoading = true;
+	// 	// Simulate loading more items (you can replace this with an actual API call)
+	// 	await new Promise((resolve) => setTimeout(resolve, 1000));
+	// 	const newItems = Array.from({ length: 5 }, (_, i) => `Item ${items.length + i + 1}`);
+	// 	items = [...items, ...newItems];
+	// 	isLoading = false;
+	// }
 
-	// Detect when the user reaches the bottom of the list
-	// @ts-ignore
-	function handleScroll(event) {
-		const { scrollTop, clientHeight, scrollHeight } = event.target;
-		if (scrollHeight - scrollTop === clientHeight) {
-			loadMoreItems();
-		}
-	}
+	// // Detect when the user reaches the bottom of the list
+	// // @ts-ignore
+	// function handleScroll(event) {
+	// 	const { scrollTop, clientHeight, scrollHeight } = event.target;
+	// 	if (scrollHeight - scrollTop === clientHeight) {
+	// 		loadMoreItems();
+	// 	}
+	// }
 
-	// Add an event listener for scrolling
-	onMount(() => {
-		const listContainer = document.querySelector('.list-container');
-		listContainer?.addEventListener('scroll', handleScroll);
-		return () => {
-			listContainer?.removeEventListener('scroll', handleScroll);
-		};
-	});
+	// // Add an event listener for scrolling
+	// onMount(() => {
+	// 	const listContainer = document.querySelector('.list-container');
+	// 	listContainer?.addEventListener('scroll', handleScroll);
+	// 	return () => {
+	// 		listContainer?.removeEventListener('scroll', handleScroll);
+	// 	};
+	// });
 </script>
 
 <div class="flex items-center justify-center">
 	<div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto w-full max-w-screen-lg p-8">
-		{#each items as item (item)}
+		{#each data.mints as mint (mint.id)}
 			<Card.Root class="bg-opacity-20 backdrop-blur-lg bg-green-40 h-120">
 				<Card.Header>
-					<Card.Title class="text-white">NFT Name</Card.Title>
+					<Card.Title class="text-white">{mint.name}</Card.Title>
 					<Card.Description class="text-green-400 overflow-ellipsis"
-						><p class="text-green-400 overflow-ellipsis">Description</p></Card.Description
+						>
+						{#if mint.description===null}
+						<p class="text-gray-400 overflow-ellipsis italic">
+							No description provided		
+						</p>
+						{:else}
+						<p class="text-green-400 overflow-ellipsis">
+							{mint.description}
+						</p>
+							{/if}
+						</Card.Description
 					>
 				</Card.Header>
 				<Card.Content class="h-4/5 overflow-hidden pb-10">
 					<!-- svelte-ignore a11y-img-redundant-alt -->
 					<img
-						src="https://images.pexels.com/photos/18015250/pexels-photo-18015250/free-photo-of-the-tallest-stone-sculpture-in-europe.jpeg"
-						alt="Image with text at the bottom right"
+						src={mint.image_url}
+						alt="mint image"
 						class="w-full h-full object-contain m-0 p-0"
 					/>
-					<p class="text-green-400 text-right pt-3">03/09/2023 05:36 PM</p>
+					<p class="text-green-400 text-right pt-3">{mint.created_at}</p>
 				</Card.Content>
 			</Card.Root>
 		{/each}
-		{#if isLoading}
+		<!-- {#if isLoading}
 			<div class="bg-black p-4 rounded-md text-green-400">Loading...</div>
-		{/if}
+		{/if} -->
 	</div>
 </div>
