50638beb4acaa17b837ed33a7098cf5202c917d6
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-10 20:30:29 +0530

    feat: homepage revamp
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/(private)/dashboard/+page.svelte
+++ src/routes/(private)/dashboard/+page.svelte
@@ -77,7 +77,7 @@
 								>
 									<div class="grid w-full items-center gap-4 text-white">
 										<div class="flex flex-col space-y-1.5">
-											<Label for="name">Name <span>*</span></Label>
+											<Label for="name">Name <span class="text-red-400">*</span></Label>
 											<Input id="name" name="projectName" placeholder="Name of your project" />
 										</div>
 										<div class="flex flex-col space-y-1.5">
@@ -89,7 +89,7 @@
 											/>
 										</div>
 										<div class="flex flex-col space-y-1.5">
-											<Label for="image">Image <span>*</span></Label>
+											<Label for="image">Image <span class="text-red-400">*</span></Label>
 											<Input
 												id="image"
 												type="file"
