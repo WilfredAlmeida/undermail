38d8285eb621dc9415606a06a651b60f0d80e751
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-09-30 23:41:57 +0530

    feat: payment db update
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/(private)/api/stripe/checkout/+server.ts
+++ src/routes/(private)/api/stripe/checkout/+server.ts
@@ -1,11 +1,16 @@
 import Stripe from "stripe";
 import { STRIPE_KEY, STRIPE_VERSION } from '$env/static/private';
+import { vercelDb } from "$lib/vercel.js";
 
 const stripe = new Stripe(STRIPE_KEY, {
 	apiVersion: '2023-08-16', // Errors when given env variable
   });
 
-export const POST = async ({ request, url }) => {
+export const POST = async ({ request, url, locals }) => {
+
+    const {
+		user: { email }
+	} = await locals.getSession();
 
     const body = await request.json();
 
@@ -28,13 +33,14 @@ export const POST = async ({ request, url }) => {
             quantity: 1,
         }],
         mode: "payment",
-        success_url: `${url.origin}/api/stripe/success`,
-        cancel_url: `${url.origin}/api/stripe/cancel`
+        success_url: `${url.origin}/account/billing`,
+        cancel_url: `${url.origin}/account/billing`
     })
 
-    console.log("SESSION");
-    console.log(JSON.stringify(session));
+    // console.log("SESSION");
+    // console.log(JSON.stringify(session));
     
+    vercelDb.set(session.id, JSON.stringify({credits, email}))
 
     return new Response(JSON.stringify({url: session.url}), {status: 200})
 }
--- src/routes/(private)/api/stripe/success/+server.ts
+++ src/routes/(private)/api/stripe/success/+server.ts
@@ -1,14 +1,35 @@
+import { vercelDb } from "$lib/vercel.js"
 import { redirect } from '@sveltejs/kit'
 
-export const GET = async({request})=>{
+export const POST = async({request, locals})=>{
 
-    // const body = await request.json()
-    
-    // console.log("IN SUCCESS");
-    // console.log(JSON.stringify(request));
+    const {supabase} = locals;
+    const body = await request.json();
 
-    throw redirect(307, "/account/billing")
+    const {data:{object:{id, amount_subtotal}}} = body
+
+    const dbSession = await vercelDb.get<string>(id)
+
+    console.log("DB SESSION");
+    console.log(JSON.stringify(dbSession));
+
+    // if(!dbSession) return new Response(JSON.stringify({message: "Session not found"}), {status: 404})
+
+    const {credits, email} = JSON.parse(JSON.stringify(dbSession!));
+    console.log(credits, email);
     
-    return new Response()
+
+    const {data, error} = await supabase.rpc("update_credits", {toadd: credits, useremail:email})
+
+    if(error) console.log(error);
+
+    console.log("DATA");
+    console.log(JSON.stringify(data));
+
+    vercelDb.del(id)
+    
+    // throw redirect(307, "/account/billing")
+
+    return new Response(JSON.stringify({message: "Success"}), {status: 200})
 
 }
