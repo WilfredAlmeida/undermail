7071246f7f026534348defeed8d2f2d3013963b4
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-10-11 12:07:11 +0530

    refactor: meta tags 4
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,11 +35,11 @@
 
 <svelte:head>
 
-<meta name="description" content="UnderMail: MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
+<meta name="description" content="MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
 <meta name="image" content="https://www.undermail.live/og.jpg" />
 
 <meta property="og:title" content="UnderMail" />
-<meta property="og:description" content="UnderMail: MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
+<meta property="og:description" content="MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
 <meta property="og:image" content="https://www.undermail.live/og.jpg" />
 <meta property="og:url" content="https://undermail.live" />
 <meta property="og:type" content="website" />
@@ -47,14 +47,14 @@
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:creator" content="undermail_" />
 <meta name="twitter:title" content="UnderMail" />
-<meta name="twitter:description" content="UnderMail: MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
+<meta name="twitter:description" content="MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT" />
 <meta name="twitter:image" content="https://www.undermail.live/og.jpg" />
 
 <meta name="keywords" content="UnderMail, Solana, Mint NFTs, cNFT, Compressed NFT, Solana Compressed NFT, Mint NFT, Mint cNFT, Mint Compresses NFT Solana, bulk, bulk mint nft, bulk mint cft, bulk mint compressed nft, nft analytics, solana analytics, cnft analytics, solana cnft analytics"/>
 
 <meta name="format-detection" content="telephone=no"/>
 <meta name="author" content="UnderMail"/>
-<meta name="description" property="og:description" content="UnderMail: MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT"/>
+<meta name="description" property="og:description" content="MailChimp of Solana | Bulk Mint and get Analytics for NFTs | UnderDog Protocol | Solana cNFT | NFT"/>
 <meta property="og:title" content="UnderMail" />
 <meta property="og:url" content="https://www.undermail.live" />
 <meta property="og:type" content="website" />
